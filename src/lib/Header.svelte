aacd213f3f4d02711541566bd49ca875b4747a3e
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-07-04 11:01:38 +0900

    2 files modified, 2 added, 2 deleted

---
+++ src/lib/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+  
+</script>
+
+<header>
+  <h1>Svelte Docs</h1>
+</header>
+
+<style>
+h1 {
+  margin: 0;
+  color: #0f0f0f;
+}
+
+header {
+  width: 100vw;
+  height: auto;
+}
+</style>
