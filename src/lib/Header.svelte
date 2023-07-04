a061e4205b6c846a4c9a9b2c1bd11fa6c7919c69
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-07-04 22:46:03 +0900

    2 files modified, 1 added

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,29 +1,79 @@
 <script>
   // Lite <=> Dark
   import LiteMode from '../assets/sun.svg'
+  import DarkMode from "../assets/moon.svg"
+  import { writable } from 'svelte/store';
+
+  import { onMount } from 'svelte';
   
   function toTop() {
     location.href = "/";
   }
 
-  if (!localStorage.getItem("_mode")) {
-    localStorage.setItem("_mode", "Lite")
+  const mode = writable(localStorage.getItem("_mode") || "Lite");
+
+  let ModeImg;
+
+  
+  
+ onMount(() => {
+  let themeStyle = document.querySelector("[docs-theme]");
+  if (!themeStyle) {
+    themeStyle = document.createElement("style");
+    themeStyle.setAttribute("docs-theme", "");
+    document.head.appendChild(themeStyle);
+  }
+});
+
+function DarkView() {
+  let themeStyle = document.querySelector("[docs-theme]");
+  if (themeStyle) {
+    themeStyle.innerHTML = `
+      body {
+        color: #fff !important;
+        background-color: #2c3e49 !important;
+        transition: .25s;
+      }
+    `;
+  }
+}
+
+function LiteView() {
+  let themeStyle = document.querySelector("[docs-theme]");
+  if (themeStyle) {
+    themeStyle.innerHTML = `
+      body {
+        color: #2c3e49 !important;
+        background-color: #fff !important;
+        transition: .25s;
+      }
+    `;
+  }
+}
+
+  $: {
+    if ($mode === "Lite") {
+      ModeImg = LiteMode;
+      LiteView()
+    } else {
+      ModeImg = DarkMode;
+      DarkView()
+    }
   }
 
-  let _mode = localStorage.getItem("_mode");
-
-  if (_mode == "Lite") {
-    // Lite view
-  }else {
-    // Dark view
-  }
-
+  const changeMode = () => {
+    mode.update((value) => {
+      const newMode = value === "Lite" ? "Dark" : "Lite";
+      localStorage.setItem("_mode", newMode);
+      return newMode;
+    });
+  };
 </script>
 
 <header>
   <h1 on:click={toTop}>Svelte Docs</h1>
   <nav>
-  <img src={LiteMode} alt="LiteIcon" height="30" class="icon">
+    <img src={ModeImg} on:click={changeMode} alt="LiteIcon" height="30" class="icon">
     <a href="/ref">
       Reference
     </a>
@@ -35,41 +85,41 @@
 <hr color="#2c3e50"/>
 
 <style>
-h1 {
-  margin: 0;
-  padding: 10px 18px 0px 10px;
-  font-size: 1.25rem;
-  font-weight: 600;
-  opacity: 0.95;
-}
+  h1 {
+    margin: 0;
+    padding: 10px 18px 0px 10px;
+    font-size: 1.25rem;
+    font-weight: 600;
+    opacity: 0.95;
+  }
 
-header {
-  width: 100vw;
-  height: auto;
-  display: flex;
-}
+  header {
+    width: 100vw;
+    height: auto;
+    display: flex;
+  }
 
-hr {
-  height: 0.3px;
-  opacity: 0.15;
-}
+  hr {
+    height: 0.3px;
+    opacity: 0.15;
+  }
 
-nav {
-  display: flex;
-  margin-right: 25px;
-  margin-left: auto;
-}
+  nav {
+    display: flex;
+    margin-right: 25px;
+    margin-left: auto;
+  }
 
-nav a {
-  color: inherit;
-  text-decoration: none;
-  font-weight: 600;
-  font-size: 0.9rem;
-  padding: 14px 8px 0px 8px;
-}
+  nav a {
+    color: inherit;
+    text-decoration: none;
+    font-weight: 600;
+    font-size: 0.9rem;
+    padding: 14px 8px 0px 8px;
+  }
 
-.icon {
-  margin-top: 8px;
-  margin-right: 3px;
-}
+  .icon {
+    margin-top: 8px;
+    margin-right: 3px;
+  }
 </style>
