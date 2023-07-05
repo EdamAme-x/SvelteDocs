3cfd6ea03aac9dbd33bef6162f501c1f13c8f1e3
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2023-07-05 17:59:10 +0900

    2 files modified

---
+++ src/ref/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  import svelteLogo from '../assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Header from "../lib/Header.svelte"
+  import Footer from "../lib/Footer.svelte"
+</script>
+
+
+<Header />
+<main>
+  aaa
+</main>
+<Footer />
+
+<style>
+    main {
+      width: 100vw;
+    }
+</style>
